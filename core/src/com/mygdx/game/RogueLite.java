77565844e776544a7255199dd5c2977af74dcfac
3DSMatt <matthew.m.watt@gmail.com>
2016-11-19 15:26:14 +0000

    Fixed duplication of "Game"
    Renamed Game.java to RogueLite.java
    Edited DesktopLauncher.java to point to the right file/class

--- core/src/com/mygdx/game/RogueLite.java
+++ core/src/com/mygdx/game/RogueLite.java
@@ -1,12 +1,12 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Game extends ApplicationAdapter {
+public class RogueLite extends Game {
 	SpriteBatch batch;
 	Texture img;
 	private Player player = new Player();
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.Game;
+import com.mygdx.game.RogueLite;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+		new LwjglApplication(new RogueLite(), config);
 	}
 }
