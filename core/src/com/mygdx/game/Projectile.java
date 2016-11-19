ceea6167d7d21a1a19d412aacdd4c4681eab4c00
3DSMatt <matthew.m.watt@gmail.com>
2016-11-19 22:16:48 +0000

    Projectile image
    In theory, added bullet.png to the projectile

--- core/src/com/mygdx/game/Projectile.java
+++ core/src/com/mygdx/game/Projectile.java
@@ -1,5 +1,7 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Interpolation;
@@ -8,7 +10,7 @@ import com.badlogic.gdx.math.Vector3;
 /**
  * Created by Matt, 19/11/2016
  */
-public class Projectile {
+public class Projectile extends ApplicationAdapter {
     private Vector3 position;
     private float speed;
     private Vector3 target;
@@ -17,29 +19,41 @@ public class Projectile {
 
 
     //Constructor: needs the position of the player, a target, & projectile speed
-    public Projectile(Vector3 currentPos,Vector3 targetPos, float s){
+    public Projectile(Vector3 currentPos, Vector3 targetPos, float s) {
         position = currentPos;
         target = targetPos;
         speed = s;
     }
 
-    public void update(){
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture(Gdx.files.internal("textures/bullet.png"));
+    }
+
+    @Override
+    public void render() {
+        batch.begin();
+        batch.draw(img, position.x, position.y);
+    }
+
+    public void update() {
         position = position.interpolate(target, speed, Interpolation.linear);
     }
 
-    public void setPosition (Vector3 pos){
+    public void setPosition(Vector3 pos) {
         position = pos;
     }
 
-    public Vector3 getPosition(){
+    public Vector3 getPosition() {
         return position;
     }
 
-    public void setSpeed(float vel){
+    public void setSpeed(float vel) {
         speed = vel;
     }
 
-    public float getSpeed(){
+    public float getSpeed() {
         return speed;
     }
 
