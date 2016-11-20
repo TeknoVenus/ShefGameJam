ac4bdcf141621639d4cd9a41f3a31a4038743b77
donnz97 <justinonn@gmail.com>
2016-11-20 15:39:18 +0000

    I could've sworn that I made the background song loop before

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -70,6 +70,7 @@ public class MainMenuScreen implements Screen {
     public void show() {
         music = Gdx.audio.newMusic(Gdx.files.internal("music/gamemusic.mp3"));
         music.play();
+        music.setLooping(true);
         //Create Table
         mainTable = new Table();
         //Set table to fill stage
