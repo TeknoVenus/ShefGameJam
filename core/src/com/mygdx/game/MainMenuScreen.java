ceaddaf5fb5d61903877201c7c8f623f9cb6548c
Wispeh <will0humph@gmail.com>
2016-11-20 13:06:06 +0000

    Menu screen fixed
    Added broken doors and fixed the invisible button bug for the second
    time.

--- core/src/com/mygdx/game/MainMenuScreen.java
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -51,40 +51,57 @@ public class MainMenuScreen implements Screen {
         		RoomRepresentation.getWindowSize(), camera);
         viewport.apply();
         stage = new Stage(viewport, game.batch);
-    }
 
-    public void create(){
-        Music music = Gdx.audio.newMusic(Gdx.files.internal("music/mainmenumusic.mp3"));
+
     }
 
 
     @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.input.setInputProcessor(stage);
+        stage.act();
+        stage.draw();
+
+    }
+
+    @Override
     public void show() {
+        music = Gdx.audio.newMusic(Gdx.files.internal("music/mainmenumusic.mp3"));
+        music.play();
         //Create Table
-        Table mainTable = new Table();
+        mainTable = new Table();
         //Set table to fill stage
         mainTable.setFillParent(true);
         //Set alignment of contents in the table.
         mainTable.center();
 
         //Create buttons
-        TextButton playButton = new TextButton("Play", skin);
-        TextButton exitButton = new TextButton("Exit", skin);
+        playButton = new TextButton("Play", skin);
+        exitButton = new TextButton("Exit", skin);
 
-        //Add listeners to buttons
-        playButton.addListener(new ClickListener(){
+        playListener = new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
-               game.setScreen(new GameScreen(game));
-               dispose();
-            }
-        });
-        exitButton.addListener(new ClickListener(){
+                game.setScreen(new GameScreen(game));
+                dispose();
+                hide();
+                mainTable.remove();
+            }};
+        quitListener = new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.app.exit();
-            }
-        });
+                dispose();
+                hide();
+                mainTable.remove();
+            }};
+
+        //Add listeners to buttons
+        playButton.addListener(playListener);
+
+        exitButton.addListener(quitListener);
 
         playButton.getLabel().setFontScale(1.3f, 1.3f);
         exitButton.getLabel().setFontScale(1.3f, 1.3f);
@@ -98,20 +115,6 @@ public class MainMenuScreen implements Screen {
         //Add table to stage
         stage.addActor(mainTable);
 
-        //Add menu music
-        music = Gdx.audio.newMusic(Gdx.files.internal("music/mainmenumusic.mp3"));
-    }
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(.1f, .12f, .16f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        Gdx.input.setInputProcessor(stage);
-        stage.act();
-        stage.draw();
-        music.play();
-        music.setLooping(true);
-
     }
 
     @Override
@@ -121,6 +124,8 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void hide() {
+        playButton.removeListener(quitListener);
+        playButton.removeListener(playListener);
     }
 
     @Override
@@ -134,5 +139,7 @@ public class MainMenuScreen implements Screen {
     @Override
     public void dispose() {
         music.dispose();
+        playButton.removeListener(quitListener);
+        playButton.removeListener(playListener);
     }
 }
