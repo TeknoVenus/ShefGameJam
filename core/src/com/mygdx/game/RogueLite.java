39d5d871381e4a609b837b1473d7190587ffb9f0
3DSMatt <matthew.m.watt@gmail.com>
2016-11-20 09:55:00 +0000

    Fixed missing import

--- core/src/com/mygdx/game/RogueLite.java
+++ core/src/com/mygdx/game/RogueLite.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
